3d86a45666a2f698f76fe0b8f107496c18b3925f
Richard Prandt <rprandt.contato@gmail.com>
2023-04-18 12:01:22 -0300

    chore: eslint rules add

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,6 +19,7 @@ module.exports = {
     "quotes": ["error", "double"],
     "semi": ["error", "always"],
     "no-undef": "off",
+    "indent": ["error", 2],
   },
   parserOptions: {
     ecmaVersion: "latest"
