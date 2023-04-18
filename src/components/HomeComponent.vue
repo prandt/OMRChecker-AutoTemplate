716e4486befa1dc2b3f742c06de2de054d30fc28
Richard Prandt <rprandt.contato@gmail.com>
2023-04-18 11:12:45 -0300

    chore: add tailwindcss

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,11 +1,6 @@
 <template>
     <div>
-        <v-slider
-        label="Number of questions"
-        class="align-center"
-        hide-details
-      >
-      </v-slider>
+        <v-btn color="primary">Generate template</v-btn>
         <v-btn color="primary">Generate template</v-btn>
     </div>
 </template>
